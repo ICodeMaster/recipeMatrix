6a663d93b3c339416cc45ba871a54424c9087239
ICodeMaster <tyleryany@gmail.com>
2021-06-15 16:51:30 -0400

    Get sub components functional

--- src/main.py
+++ src/main.py
@@ -16,6 +16,10 @@ masterRecipe = r.Recipe("Tandaloo")
 masterRecipe.addRecipeIngredient(3, "lb", porkIngrd)
 masterRecipe.addRecipeIngredient(6, "cup", yougurtIngrd)
 masterRecipe.addRecipeIngredient(500, "g", cukeIngrd)
+subRecipe = r.Recipe("Saucy-Sauce")
+subRecipe.addRecipeIngredient(6, "g", yougurtIngrd)
+subRecipe.addRecipeIngredient(40, "oz", cukeIngrd)
+masterRecipe.addRecipeIngredient(40, "oz", subRecipe)
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -26,17 +30,21 @@ class Application(tk.Frame):
         self.create_widgets()
         self.loadRecipe(masterRecipe)
     class FrameEntry(tk.Frame):
-        def __init__(self, arrayIngredient: r.RecipeIngredient, master=None):
+        def __init__(self, arrayIngredient: r.RecipeComponent, master=None):
             super().__init__(master)
             #self.pack()
             self.master = master
             self.ingredient = arrayIngredient
+            self.frames = []
             self.entry_name = tk.Label(self)
             self.entry_name["text"] = str(arrayIngredient.ingredient.name) + ":" + str(arrayIngredient.amount) + str(arrayIngredient.measureType)
             self.button_remove = tk.Button(self, text="Remove")
             self.button_remove["command"] = self.removeButtonClick
-            self.entry_name.pack()
-            self.button_remove.pack()
+            self.entry_name.grid(row = 0, column=0, padx=0, pady=5)
+            self.button_remove.grid(row = 0, column=1, padx=0, pady=5)
+            if arrayIngredient.isRecipe():
+                self.createSubRecipeFrames(arrayIngredient.ingredient)
+                self.currentIngredients = arrayIngredient.ingredient.componentList
         def removeButtonClick(self):
             try:
                 ### Find yourself in master array
@@ -47,26 +55,33 @@ class Application(tk.Frame):
                 self.destroy()
             except:
                 print("fail remove" + str(self))
+        def createSubRecipeFrames(self, recipe):
+            for i, currentIngredient in enumerate(recipe.componentList, start=1):
+                frame_entry = self.__class__(currentIngredient, master=self)
+                frame_entry.grid(sticky="ew",row = i, column=1, padx=0, pady=5)
+                frame_entry["bg"] = "red"
+                self.frames.append(frame_entry)
+
     def loadRecipe(self, recipe: r.Recipe):
         self.recipeNameEntry["text"] = recipe.name
-        self.currentIngredients = recipe.ingredients
+        self.currentIngredients = recipe.componentList
         self.populateIngredients(recipe)
     def create_widgets(self):
         self.recipeNameEntry = tk.Label(self)
-        self.recipeNameEntry.grid(row = 1, column=1 )
+        self.recipeNameEntry.grid(row = 1, column=0 )
         #self.hi_there.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.grid(row = 1, column = 0)
+        self.quit.grid(row = 1, column = 1)
     def populateIngredients(self, recipe: r.Recipe):
         for i in self.frames:
             self.frames[i].destroy()
         self.frames.clear()
-        for i, currentIngredient in enumerate(recipe.ingredients, start=1):
+        for i, currentIngredient in enumerate(recipe.componentList, start=1):
             frame_entry = self.FrameEntry(currentIngredient, master=self)
             frame_entry.grid(sticky="ew",row = i+2, column=0, padx=0, pady=5)
+            frame_entry["bg"] = "blue"
             self.frames.append(frame_entry)
-            print("frame created")
 
 root = tk.Tk()
 app = Application(master=root)
--- src/recipe/__init__.py
+++ src/recipe/__init__.py
@@ -1,26 +1,37 @@
 from component import ingredient as i
 
 
-class RecipeIngredient():
-        def __init__(self, amount, type, ingredient: i.Ingredient):
+class RecipeComponent():
+        def __init__(self, amount, type, ingredient):
             self.amount = amount
             self.ingredient = ingredient
             self.measureType = type
+        def isIngredient(self):
+            if isinstance(self.ingredient, i.Ingredient):
+                return True
+            else:
+                return False
+        def isRecipe(self):
+            if isinstance(self.ingredient, Recipe):
+                return True
+            else:
+                return False
+
 
 class Recipe:
 
-    def __init__(self, name, ingredients: list[RecipeIngredient] = []):
+    def __init__(self, name):
         self.name = name
         # TODO Alphabetize
-        self.ingredients = ingredients
+        self.componentList = []
         self.steps = []
         self.yieldAmount = None
         # Total time, active time, passive time
         self.time = []
         # TODO Add tools
     def addRecipeIngredient(self, amount, type, ingredient: i.Ingredient):
-        recipe_ingredient = RecipeIngredient(amount, type, ingredient)
-        self.ingredients.append(recipe_ingredient)
+        recipe_ingredient = RecipeComponent(amount, type, ingredient)
+        self.componentList.append(recipe_ingredient)
 
     def defineStep(self, text, ingredients: list[i.Ingredient]):
         step = RecipeStep(text, ingredients)
