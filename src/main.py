807472225b81ed226c9f9447a99587451426c147
ICodeMaster <tyleryany@gmail.com>
2021-06-16 12:17:00 -0400

    More work on data structure

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #%%
 import sys
 import tkinter as tk
-from tkinter.constants import END
+from tkinter.constants import E, END, LEFT, RIGHT, TOP, W
 sys.path.append('C:/Users/user/Documents/Recipe Do-Dad/recipeMatrix/src')
 from component import (ingredient as ig)
 import recipe as r
@@ -30,27 +30,27 @@ class Application(tk.Frame):
         self.create_widgets()
         self.loadRecipe(masterRecipe)
     class FrameEntry(tk.Frame):
-        def __init__(self, arrayIngredient: r.RecipeComponent, master=None):
+        def __init__(self, recipeComponent: r.RecipeComponent, master=None):
             super().__init__(master)
             #self.pack()
             self.master = master
-            self.ingredient = arrayIngredient
+            self.recipeComponent = recipeComponent
             self.frames = []
             self.entry_name = tk.Label(self)
-            self.entry_name["text"] = str(arrayIngredient.ingredient.name) + ":" + str(arrayIngredient.amount) + str(arrayIngredient.measureType)
+            self.entry_name["text"] = str(recipeComponent.ingredient.name) + ":" + str(recipeComponent.amount) + str(recipeComponent.measureType)
             self.button_remove = tk.Button(self, text="Remove")
             self.button_remove["command"] = self.removeButtonClick
-            self.entry_name.grid(row = 0, column=0, padx=0, pady=5)
-            self.button_remove.grid(row = 0, column=1, padx=0, pady=5)
-            if arrayIngredient.isRecipe():
-                self.createSubRecipeFrames(arrayIngredient.ingredient)
-                self.currentIngredients = arrayIngredient.ingredient.componentList
+            self.entry_name.grid(row = 0, column=0, columnspan=2, sticky="w",padx=15,pady=5)
+            self.button_remove.grid(row=0, column=2, sticky="e",padx=15,pady=5)
+            if recipeComponent.isRecipe():
+                self.createSubRecipeFrames(recipeComponent.ingredient)
+                self.currentRecipe = self.recipeComponent.ingredient
         def removeButtonClick(self):
             try:
                 ### Find yourself in master array
                 i_remove = self.master.frames.index(self)
                 ### Remove corresponding data entry
-                self.master.currentIngredients.pop(i_remove)
+                self.master.currentRecipe.componentList.pop(i_remove)
                 self.master.frames.remove(self)
                 self.destroy()
             except:
@@ -58,13 +58,14 @@ class Application(tk.Frame):
         def createSubRecipeFrames(self, recipe):
             for i, currentIngredient in enumerate(recipe.componentList, start=1):
                 frame_entry = self.__class__(currentIngredient, master=self)
-                frame_entry.grid(sticky="ew",row = i, column=1, padx=0, pady=5)
+                frame_entry.grid(column=1, row=1+i, columnspan=2, sticky="e",padx=15,pady=5)
                 frame_entry["bg"] = "red"
                 self.frames.append(frame_entry)
 
+
     def loadRecipe(self, recipe: r.Recipe):
         self.recipeNameEntry["text"] = recipe.name
-        self.currentIngredients = recipe.componentList
+        self.currentRecipe = recipe
         self.populateIngredients(recipe)
     def create_widgets(self):
         self.recipeNameEntry = tk.Label(self)
